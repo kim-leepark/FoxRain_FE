f67630ca4dd952deaeab07cc713bab819b560d2e
hyejun <hyejun0608@naver.com>
2022-05-03 16:59:49 +0900

    🔥:: admin header component

---
+++ foxrain/src/componetns/header/style.ts
@@ -0,0 +1,28 @@
+import styled from '@emotion/styled';
+
+export const AdminHeader = styled.div`
+  width: 100%;
+  height: 75px;
+  @media only screen and (max-width: 1440px) {
+    height: 55px;
+  }
+  > div {
+    width: 55.5%;
+    min-width: 800px;
+    height: 100%;
+    margin: 0 auto;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    > p:last-child {
+      cursor: pointer;
+    }
+  }
+`;
+
+export const Text = styled.p`
+  font-size: 1.5rem;
+  @media only screen and (max-width: 1440px) {
+    font-size: 1.125rem;
+  }
+`;
