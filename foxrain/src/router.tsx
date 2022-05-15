a7894e403bd8cdceee035c53c2c341acfd53440e
kky04994 <kkyo8747@naver.com>
2022-05-15 22:41:27 +0900

    🔥:: add main router

--- foxrain/src/router.tsx
+++ foxrain/src/router.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Route, Routes, BrowserRouter } from 'react-router-dom';
+import { Main } from './componetns';
 
 const Router = (): JSX.Element => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<></>}></Route>
+        <Route path='/' element={<Main />}></Route>
       </Routes>
     </BrowserRouter>
   );
