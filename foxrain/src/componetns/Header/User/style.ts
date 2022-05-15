4fac91c8c1a18778788ee5e07e506f6e6215f833
kky04994 <kkyo8747@naver.com>
2022-05-15 22:43:08 +0900

    🔥:: add user header

---
+++ foxrain/src/componetns/Header/User/style.ts
@@ -0,0 +1,39 @@
+import styled from '@emotion/styled';
+
+export const Container = styled.div`
+  height: 75px;
+  width: 100%;
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: 100;
+`;
+
+export const Content = styled.div`
+  margin: 0 auto;
+  width: 1280px;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+export const Logo = styled.img``;
+
+export const NavBox = styled.div`
+  width: 345px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+export const Font = styled.div`
+  font-family: 'Roboto';
+  font-size: 20px;
+  color: #ffffff;
+  cursor: pointer;
+`;
+
+export const Profile = styled.img`
+  cursor: pointer;
+`;
