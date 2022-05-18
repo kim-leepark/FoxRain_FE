b67dc8cc9ceda6eaa096695ef1bf64b285b92c23
kky04994 <kkyo8747@naver.com>
2022-05-18 14:09:37 +0900

    🔥:: 각각의 컴포넌트 post 폴더에 style 파일 추가

---
+++ foxrain/src/componetns/Main/Post/style.ts
@@ -0,0 +1,17 @@
+import styled from '@emotion/styled';
+
+export const Post = styled.div`
+  width: 1280px;
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-gap: 20px;
+  justify-items: center;
+  gap: 20px;
+  margin: 0 auto;
+  margin-top: 39px;
+  @media only screen and (max-width: 1440px) {
+    width: 960px;
+    gap: 15px;
+    margin-top: 29px;
+  }
+`;
