7a95d1f742508d2dca43a9311575faf7ca1f23cf
hyejun <hyejun0608@naver.com>
2022-05-20 15:59:35 +0900

    🔥:: mypage components

---
+++ foxrain/src/componetns/Mypage/index.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import * as S from './style';
+import Header from '../Header/User';
+import { banner } from '../../assets/mypage';
+import Post from './post';
+
+const Mypage = () => {
+  return (
+    <S.Mypage>
+      <Header />
+      <S.BannerContent>
+        <img src={banner} alt='banner' />
+        <p>
+          김혜준님 <br /> 모두가 잠든 밤에 피는 꽃도 있습니다.
+        </p>
+      </S.BannerContent>
+      <Post />
+    </S.Mypage>
+  );
+};
+
+export default Mypage;
