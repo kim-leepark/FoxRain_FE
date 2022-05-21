bd6ddadee662218498d053d3350cdb0068c77d6d
kky04994 <kkyo8747@naver.com>
2022-05-22 04:06:54 +0900

    🔥:: 글자에 nav 링크 연결

--- foxrain/src/componetns/Header/User/index.tsx
+++ foxrain/src/componetns/Header/User/index.tsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import * as S from './style';
 import Profile from '../../../assets/Header/Profile.svg';
+import { useNavigate } from 'react-router';
 
 const Header = () => {
+  const navigate = useNavigate();
+
   return (
     <S.Container>
       <S.Content>
-        <div>로고 위치</div>
+        <div onClick={() => navigate(`/`)}>로고 위치</div>
         <S.NavBox>
           <S.Font>글귀 작성하기</S.Font>
           <S.Font>게시글 작성하기</S.Font>
-          <S.Profile src={Profile} />
+          <S.Img src={Profile} onClick={() => navigate(`/mypage`)} />
         </S.NavBox>
       </S.Content>
     </S.Container>
