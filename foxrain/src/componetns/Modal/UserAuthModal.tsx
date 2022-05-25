e99be41186dbc5971496362630670eb24b8e3947
hyejun <hyejun0608@naver.com>
2022-05-25 14:51:06 +0900

    🔥:: UserAuthModal 유저 로그인 모달

---
+++ foxrain/src/componetns/Modal/UserAuthModal.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import * as S from './style';
+
+const UserAuthModal = () => {
+  return (
+    <S.Modal width={600} height={360}>
+      <S.ModalText>
+        <span>로그인</span>을 해주세요!
+      </S.ModalText>
+      <S.Input width={440} placeholder={'이메일을 입력해 주세요.'} />
+      <S.Input width={440} placeholder={'비밀번호를 입력해 주세요.'} />
+      <S.Button width={440}>로그인</S.Button>
+    </S.Modal>
+  );
+};
+
+export default UserAuthModal;
