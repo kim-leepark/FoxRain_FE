ace09d8de342ccbe811a04911a8f95f1f3ebfdb6
hyejun <hyejun0608@naver.com>
2022-06-02 23:29:36 +0900

    🔥:: mypage api 연동

--- foxrain/src/componetns/Mypage/index.tsx
+++ foxrain/src/componetns/Mypage/index.tsx
@@ -1,20 +1,44 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import * as S from './style';
 import Header from '../Header/User';
 import { banner } from '../../assets/mypage';
 import Post from './post';
+import { getRequestWithAccessToken } from '../../api';
 
 const Mypage = () => {
+  const token = localStorage.getItem('accessToken');
+  const [feed, setFeed] = useState<
+    Array<{
+      postId: number;
+      title: string;
+      content: string;
+    }>
+  >();
+  const [userName, setUserName] = useState<string>('');
+
+  useEffect(() => {
+    const request = getRequestWithAccessToken(token ? token : '', 2);
+    request
+      .get('/user')
+      .then(response => {
+        setFeed(response.data);
+        setUserName(response.data[0].name);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }, []);
+
   return (
     <S.Mypage>
       <Header isMain={false} />
       <S.BannerContent>
         <img src={banner} alt='banner' />
         <p>
-          김혜준님 <br /> 모두가 잠든 밤에 피는 꽃도 있습니다.
+          {userName}님 <br /> 모두가 잠든 밤에 피는 꽃도 있습니다.
         </p>
       </S.BannerContent>
-      <Post />
+      <Post content={feed ? feed : null} />
     </S.Mypage>
   );
 };
