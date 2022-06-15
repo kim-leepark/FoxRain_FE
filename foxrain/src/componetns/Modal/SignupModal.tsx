c18e666f87e5e51aa65e18d948eaed96a9fb8b8d
kky04994 <kkyo8747@naver.com>
2022-06-15 15:01:53 +0900

    🔥:: 필요없는 console.log 코드 삭제

--- foxrain/src/componetns/Modal/SignupModal.tsx
+++ foxrain/src/componetns/Modal/SignupModal.tsx
@@ -12,8 +12,6 @@ const SignupModal : FC<Props> = props => {
   const [name, setUserName] = useState("")
   const [password, setUserPassword] = useState("")
 
-  console.log(email, name, password)
-
   const onSucess = () => {
     alert("회원가입이 완료되었습니다.");
     window.location.replace("/");
