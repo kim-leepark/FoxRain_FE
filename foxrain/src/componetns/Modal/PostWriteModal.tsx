1c6eb6a69c9d98cbb3e6435458380de959b6bfd6
hyejun <hyejun0608@naver.com>
2022-06-17 11:54:35 +0900

    🔥:: 입력시 상태 변경 & 게시글 작성 api 연동

--- foxrain/src/componetns/Modal/PostWriteModal.tsx
+++ foxrain/src/componetns/Modal/PostWriteModal.tsx
@@ -1,4 +1,5 @@
-import React, { Dispatch, FC, SetStateAction } from 'react';
+import React, { Dispatch, FC, SetStateAction, useState } from 'react';
+import { getRequestWithAccessToken } from '../../api';
 import * as S from './style';
 
 interface Props {
@@ -7,6 +8,35 @@ interface Props {
 
 const PostWriteModal: FC<Props> = props => {
   const { showModal } = props;
+  const [title, setTitle] = useState<string>('');
+  const [content, setContent] = useState<string>('');
+  const token = localStorage.getItem('accessToken');
+
+  const titleChangeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setTitle(event.currentTarget.value);
+  };
+
+  const contentChangeHandler = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setContent(event.currentTarget.value);
+  };
+
+  const uploadBtnClickHandler = () => {
+    const request = getRequestWithAccessToken(token ? token : '', 1);
+    request
+      .post('/post', {
+        title: title,
+        content: content,
+      })
+      .then(response => {
+        if (response.status === 201) {
+          alert('게시글 작성을 성공했습니다.');
+          window.location.reload();
+        }
+      })
+      .catch(error => {
+        console.log('upload post error: ' + error);
+      });
+  };
 
   return (
     <>
@@ -17,9 +47,15 @@ const PostWriteModal: FC<Props> = props => {
           <br />
           모두에게 공유해주세요.
         </S.ModalText>
-        <S.Input width={650} placeholder={'제목을 입력해 주세요.'} />
-        <S.TextArea placeholder={'내용을 입력해 주세요.'} height={235} />
-        <S.Button width={650}>공유하기</S.Button>
+        <S.Input width={650} placeholder={'제목을 입력해 주세요.'} onChange={titleChangeHandler} />
+        <S.TextArea
+          placeholder={'내용을 입력해 주세요.'}
+          height={235}
+          onChange={contentChangeHandler}
+        />
+        <S.Button width={650} onClick={uploadBtnClickHandler}>
+          공유하기
+        </S.Button>
       </S.Modal>
     </>
   );
