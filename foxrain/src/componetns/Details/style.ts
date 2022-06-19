7854d9daf5c1e16f9bbcb90b3c3e3893454c50e0
kky04994 <kkyo8747@naver.com>
2022-06-19 23:30:18 +0900

    🔥:: 화면에 맞게 스타일 비율 조정

--- foxrain/src/componetns/Details/style.ts
+++ foxrain/src/componetns/Details/style.ts
@@ -44,25 +44,29 @@ export const FlexBox = styled.div`
   display: flex;
   margin-bottom: 19px;
   justify-content: space-between;
-`
+`;
 
 export const CommentInput = styled.input`
   background: #fbfbfb;
   border: 1px solid #c7c7c7;
   border-radius: 10px;
-  width: 1100px;
+  width: 92%;
   height: 60px;
-  padding-left: 10px;
+  padding: 10px;
+  @media only screen and (max-width: 1440px) {
+    max-width: 90%;
+  }
 `;
 
 export const CommentBtn = styled.div`
   background: #6085c8;
   border-radius: 15px;
-  width: 130px;
+  width: 10%;
   height: 60px;
-  padding: 17px 10px;
-  font-size: 20px;
+  padding-top: 19px;
+  font-size: 16px;
   margin-left: 50px;
+  text-align: center;
   cursor: pointer;
   text-align: center;
   color: #ffffff;
@@ -71,26 +75,26 @@ export const CommentBtn = styled.div`
 export const CommtentNull = styled.div`
   margin-top: 19px;
   color: #c7c7c7;
-`
+`;
 
 export const CommentContentBox = styled.div`
   min-width: 84%;
   height: fit-content;
   padding: 10px;  
+  width: 92%;
   border: 1px solid #c7c7c7;
   border-radius: 10px;
   @media only screen and (max-width: 1440px) {
-    max-width: 84.5%;
+    max-width: 90%;
   }
-`
+`;
 
-export const DeclarationBox = styled.div`
-  width: 125px;
-  height: 30px;
+export const DeclarationBox = styled(CommentBtn)`
+  height: 22px;
   color: #ffffff;
-  padding-top: 4px;
+  padding-top: 2px;
   text-align: center;
-  font-size: 16px;
+  font-size: 10px;
   background: #FF0000;
   border-radius: 10px;
 `;
